4b9d3b282aa6011c5cebbcd8a583fba06e94fb93
nahee kim <nahbee10@naver.com>
2020-10-21 17:26:37 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5cc558f524e981e4e346f4b3406f43ed.js"
+  "/precache-manifest.c7a33c579080e44d48bfa1fc53a60a30.js"
 );
 
 self.addEventListener('message', (event) => {
